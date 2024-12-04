7d69c20cc9468ca295b8f94a62f56d4d3c7799a6
Anthony Castellano <tony@MacBookPro.attlocal.net>
2024-12-03 20:15:25 -0500

    fix premature comment parsing bugs

--- scripts/content.js
+++ scripts/content.js
@@ -1,13 +1,11 @@
-const comments = document.querySelectorAll('[slot="comment"]');
-const commentTexts = [];
-for (const c of comments) {
-    const commentText = c.querySelector('div > div > p').textContent;
-    commentTexts.push(commentText);
-}
-
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.request === 'comments') {
-        // sendResponse(commentTexts);
-        sendResponse('test');
+        const comments = document.querySelectorAll('[slot="comment"]');
+        const commentTexts = [];
+        for (const c of comments) {
+            const commentText = c.querySelector('div > div > p').textContent.trim();
+            commentTexts.push(commentText);
+        }
+        sendResponse(commentTexts);
     }
 });
